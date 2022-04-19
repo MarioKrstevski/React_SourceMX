80950e0da4c6c7ac45895da3c4da9605496b4f3f
Mario Krstevski <mariokrstevski@hotmail.com>
2022-04-19 18:59:04 +0200

    variation with strings

--- src/exercises/Tabs.js
+++ src/exercises/Tabs.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 export default function Tab() {
-  const [activeTab, setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState("m");
 
   //   let mapping = {
   //     1: <div class="tab-pane">Contents for tab 1</div>,
@@ -18,15 +18,15 @@ export default function Tab() {
     <div className="tab-component">
       <div id="tabs-items">
         <ul>
-          <li onClick={() => setActiveTab(1)}>Tab 1</li>
-          <li onClick={() => setActiveTab(2)}>Tab 2</li>
-          <li onClick={() => setActiveTab(3)}>Tab 3</li>
+          <li onClick={() => setActiveTab("m")}>Mario</li>
+          <li onClick={() => setActiveTab("s")}>Stefan</li>
+          <li onClick={() => setActiveTab("t")}>Tamara</li>
         </ul>
       </div>
       <div id="tabs-content">
-        {activeTab === 1 && <div class="tab-pane">Contents for tab 1</div>}
-        {activeTab === 2 && <div class="tab-pane">Contents for tab 2</div>}
-        {activeTab === 3 && <div class="tab-pane">Contents for tab 3</div>}
+        {activeTab === "m" && <div class="tab-pane">Contents for Mario </div>}
+        {activeTab === "s" && <div class="tab-pane">Contents for Stefan</div>}
+        {activeTab === "t" && <div class="tab-pane">Contents for Tamara</div>}
       </div>
 
       {/* <div id="tabs-content">{renderCorrespondingTabContent(activeTab)}</div> */}
